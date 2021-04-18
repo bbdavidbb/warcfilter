2c15f6efb161b3886a09b3a99614d439f4b283be
unknown <bbdavidbb@gmail.com>
2021-04-18 15:06:56 -0700

    added mode genCCWebGraph and direct command line arguments

--- index.js
+++ index.js
@@ -10,30 +10,44 @@
 
 const readline = require('readline');
 const Main = require('./lib/Main.js');
-
- process.setMaxListeners(100);
+const WatParser = require('./lib/WatParser.js')
+const fetch = require('node-fetch');
+const zlib = require('zlib');
+let fs = require("fs");
 
 let rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
  
 
 let parser = new Main();
 mainPrompt = async () => {
-    rl.question("Enter in this format: src: origFile dest: destinationFile mode: mode {arguments}, press e to exit: \n", async function(input) {
+    rl.question("Enter in this format: origFile, destination file, {arguments}, t for a timing test or  press e to exit: \n", async function(input) {
         switch(input) {
             case "e":
             case "E":
                 rl.close();
                 process.exit(0);
             default:
-              let response = await parser.run(input)
+              await parser.run(input)
               mainPrompt();
           }
     });
 }
 
-mainPrompt();
+runArgs = async (args) => {
+    await parser.run(args);
+}
+
+
+if(process.argv.length > 2) {
+    let args = process.argv.slice(2).join(" ");
+    runArgs(args);
+}
+else {
+    mainPrompt();
+}
 
 
